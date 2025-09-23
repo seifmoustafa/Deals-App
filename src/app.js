7365117d8088fa1620184fcc8bf8d4f16f53285f
Mina Latif <minalatif18@gmail.com>
2025-09-23 21:17:51 +0300

    Fixx

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-//require('dotenv').config();
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const swaggerUi = require('swagger-ui-express');
@@ -21,45 +20,41 @@ const bookmarkRoute = require('./routes/bookmark.routes');
 
 const app = express();
 
-// Middleware
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+// 🟢 ملوتر لازم يشتغل من غير ما البودي يتاكل
+// عشان كده نخلي روتات الصور تيجي قبل parsers
+app.use('/api/users', userRoutes);
+
+// باقي الروتات تقدر تستخدم parsers عادي
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/home', homeRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/stores', storeRoutes);
 app.use('/api/coupons', couponRoutes);
-app.use('/api/users', userRoutes);
 app.use('/api/admins', adminRoutes);
 app.use('/api/announcements', announcementsRoutes);
 app.use('/api/notifications', notificationRoutes);
 app.use('/api/bookmarks', bookmarkRoute);
 
-
-app.use((req, res, next) => {
-  if (req.originalUrl.startsWith('/api/users/upload-profile')) {
-    return next(); // skip parsers for file upload
-  }
-  express.json()(req, res, next);
-});
-
-// Basic error handling middleware
+// Errors
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ status: 'error', message: 'Something went wrong!' });
 });
 
-// Health check endpoint
+// Health check
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'success', message: 'Server is healthy' });
 });
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// 404 handler
+// 404
 app.use((req, res) => {
   res.status(404).json({ status: 'error', message: 'Route not found' });
 });
